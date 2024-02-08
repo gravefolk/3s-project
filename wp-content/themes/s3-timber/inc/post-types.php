d46082f0e51b32a2f1b7660f3cb5fd90db8e6d9b
gravefolk <karolisjurgelevicius@gmail.com>
2024-02-07 19:07:16 -0500

    created products cpt + tweaked env file

--- wp-content/themes/s3-timber/inc/post-types.php
+++ wp-content/themes/s3-timber/inc/post-types.php
@@ -7,40 +7,30 @@ add_action( 'init', 'register_post_types' );
 
 function register_post_types(){
 
-	register_post_type( 'post_type_name', [
+	register_post_type( 'products', [
 		'taxonomies' => [], // post related taxonomies
 		'label'  => null,
 		'labels' => [
-			'name'               => '____', // name for the post type.
-			'singular_name'      => '____', // name for single post of that type.
-			'add_new'            => 'Add ____', // to add a new post.
-			'add_new_item'       => 'Adding ____', // title for a newly created post in the admin panel.
-			'edit_item'          => 'Edit ____', // for editing post type.
-			'new_item'           => 'New ____', // new post's text.
-			'view_item'          => 'See ____', // for viewing this post type.
-			'search_items'       => 'Search ____', // search for these post types.
+			'name'               => 'Products', // name for the post type.
+			'singular_name'      => 'Product', // name for single post of that type.
+			'add_new'            => 'Add Product', // to add a new post.
+			'add_new_item'       => 'Adding Products', // title for a newly created post in the admin panel.
+			'edit_item'          => 'Edit Product', // for editing post type.
+			'new_item'           => 'New Product', // new post's text.
+			'view_item'          => 'See Product', // for viewing this post type.
+			'search_items'       => 'Search Products', // search for these post types.
 			'not_found'          => 'Not Found', // if search has not found anything.
-			'parent_item_colon'  => '', // for parents (for hierarchical post types).
-			'menu_name'          => '____', // menu name.
+			'menu_name'          => 'Products', // menu name.
 		],
 		'description'         => '',
 		'public'              => true,
-		//'publicly_queryable'  => null, // depends on public
-		//'exclude_from_search' => null, // depends on public
-		//'show_ui'             => null, // depends on public
-		//'show_in_nav_menus'   => null, // depends on public
 		'show_in_menu'        => null, // whether to in admin panel menu
-		//'show_in_admin_bar'   => null, // depends on show_in_menu.
-		'show_in_rest'        => null, // Add to REST API. WP 4.7.
+		'show_in_rest'        => true, // Add to REST API. WP 4.7.
 		'rest_base'           => null, // $post_type. WP 4.7.
 		'menu_position'       => null,
-		'menu_icon'           => null,
-		//'capability_type'   => 'post',
-		//'capabilities'      => 'post', // Array of additional rights for this post type.
-		//'map_meta_cap'      => null, // Set to true to enable the default handler for meta caps.
+		'menu_icon'           => 'dashicons-store',
 		'hierarchical'        => false,
-		// [ 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'trackbacks', 'custom-fields', 'comments', 'revisions', 'page-attributes', 'post-formats' ]
-		'supports'            => [ 'title', 'editor' ],
+		'supports'            => [ 'title', 'editor', 'revisions' ],
 		'has_archive'         => false,
 		'rewrite'             => true,
 		'query_var'           => true,
